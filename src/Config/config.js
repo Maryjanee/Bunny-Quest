00027afcf3701cebddd4e123f42aa82213d5f095
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-12-10 16:37:49 +0100

    Add template for all scenes

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+import 'phaser';
+import GameScene from '../Scenes/GameScene';
+ 
+export default {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  
+};
---
+++ src/Scenes/OptionsScene.js
@@ -0,0 +1,14 @@
+
+import 'phaser';
+ 
+export default class OptionsScene extends Phaser.Scene {
+  constructor () {
+    super('Options');
+  }
+ 
+  preload () {
+  }
+ 
+  create () {
+  }
+};
