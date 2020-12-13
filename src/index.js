6f892ea1f0834aca687fadbb559fc5e718b1bd29
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-12-13 06:22:52 +0100

    Refactor code for button creation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'phaser';
+
 import config from './Config/config';
 import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
@@ -6,10 +7,12 @@ import PreloaderScene from './Scenes/PreloaderScene';
 import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
 import CreditsScene from './Scenes/CreditsScene';
- 
+import Model from './Model';
 class Game extends Phaser.Game {
   constructor () {
     super(config);
+    const model = new Model();
+    this.globals = { model };
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
@@ -21,3 +24,4 @@ class Game extends Phaser.Game {
 }
  
 window.game = new Game();
+
