c4449731c6ef7909c6a81b671e2c859abf2e0ff5
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-12-27 16:34:23 +0100

    Fix issue with leaderboard table display

--- src/Scenes/LeaderboardScene.js
+++ src/Scenes/LeaderboardScene.js
@@ -1,63 +1,56 @@
-import 'phaser';
-import Button from '../Objects/Button';
+import "phaser";
+import Button from "../Objects/Button"
 
-
- 
 export default class LeaderboardScene extends Phaser.Scene {
-  constructor () {
-    super('Leaderboard');
+  constructor() {
+    super("Leaderboard");
   }
-  
- 
 
-  create () {
-    this.add.text(150, 250, 'Leaderboard Page')
-    this.returnTopGamers();
-    
-}
+  create() {
+    this.add.text(150, 250, "Leaderboard Page");
+    this.retrieveUsers();
+  }
 
-
-
-  retrieveUsers(){
-      fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/iy8gwhVatNBXYEvndkqk/scores/'
+  retrieveUsers() {
+    fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/iy8gwhVatNBXYEvndkqk/scores/"
     )
-    .then(response => response.json())
-    .then(data => {
-      const allUsers = data.result
-      const topUsers = this.sortTopTenGamers(allUsers);
-      this.createLeaderBoardTable(topUsers)
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        const allUsers = data.result;
+        const topUsers = this.sortTopTenGamers(allUsers);
+        this.createLeaderBoardTable(topUsers);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
   }
-  
-  sortTopTenGamers(arr){
-   return arr.sort((a,b) => b-a).slice(0,10);
-    
+
+  sortTopTenGamers(arr) {
+    const convertedScores = arr.map(hs => ({ user: hs.user, score: Number(hs.score)}));
+    console.log("before sort", convertedScores.slice(0, 10));
+    convertedScores.sort((a, b) => b.score - a.score);
+    console.log("after sort", convertedScores.slice(0, 10));
+    return convertedScores.slice(0, 10);
   }
-  
-  createLeaderBoardTable(obj){
-    const table = document.createElement('table');
-    const tableHeadings = document.createElement('tr');
-    tableHeadings.innerHTML = `<th>Rank</th>
+
+  createLeaderBoardTable(arr) {
+    const table = document.createElement("table");
+    const tableHeadings = document.createElement("tr");
+    tableHeadings.innerHTML = `
                                <th>Username</th>
                                <th>Score</th>`;
-                              
-    table.appendChild(tableHeadings);  
-      
-    obj.forEach((user, index) =>{
-      const row = document.createElement('tr');
-      row.innerHTML = `<th>${index + 1}</th>
-      <th>${user.name}</th>
-                       <th>${user.score}</th>`;
-      table.appendChild(row);                 
-      });
-      
-      return table;
-      
+
+    table.appendChild(tableHeadings);
+
+    arr.forEach((user, index) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+      <td>${user.user}</td>
+                       <td>${user.score}</td>`;
+      table.appendChild(row);
+    });
+
+    document.querySelector('.results').appendChild(table);
   }
-  
-
-
-};
+}
